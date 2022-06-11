ebba0c0a558afab726dc969f154c267a5c222a9e
IvanIvanov <ipz214_iiyu@student.ztu.edu.ua>
2022-06-11 22:46:26 +0300

    Lab 9

---
+++ oop-lab9/ClassLibrary1/Fraction.cs
@@ -0,0 +1,78 @@
+﻿namespace ClassLibrary1
+{
+    public class Fraction
+    {
+         int num;
+         int den;
+        bool ok;
+        public Fraction(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new ArgumentException("Знаменик не може бути нулем...", nameof(y));
+            }
+            num = x;
+            den = y;
+        }
+
+        public static Fraction operator +(Fraction a) => a;
+
+        public static Fraction operator -(Fraction a) => new Fraction(-a.num, a.den);
+
+        public static Fraction operator +(Fraction a, Fraction b) => new Fraction(a.num * b.den + b.num * a.den, a.den * b.den);
+
+        public static Fraction operator -(Fraction a, Fraction b) => a + (-b);
+
+        public static Fraction operator *(Fraction a, Fraction b) => new Fraction(a.num * b.num, a.den * b.den);
+
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            if (b.num == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return new Fraction(a.num * b.den, a.den * b.num);
+        }
+        public static bool operator <(Fraction a, Fraction b)
+        {
+            a.ok = a.num * b.den < a.den * b.num;
+            return a.ok;
+
+        }
+        public static bool operator >(Fraction a, Fraction b)
+        {
+            a.ok = a.num * b.den > a.den * b.num;
+            return a.ok;
+        }
+        public static bool operator >=(Fraction a, Fraction b)
+        {
+            return a.ok = a.num * b.den >= a.den * b.num;
+        }
+        public static bool operator <=(Fraction a, Fraction b)
+        {
+            return a.ok = a.num * b.den <= a.den * b.num;
+        }
+        public static bool operator ==(Fraction a, Fraction b)
+        {
+            return a.ok = a.num * b.den == a.den * b.num;
+        }
+        public static bool operator !=(Fraction a, Fraction b)
+        {
+            return a.ok = a.num * b.den != a.den * b.num;
+        }
+        public override string ToString() => $"{num} / {den}";
+        public static void Cut(Fraction a)
+        {
+            double res = ToDouble(a);
+
+            Console.WriteLine($"{1:F}",res);
+        }
+        public static double ToDouble(Fraction a)
+        {
+            double res = (double)a.num / a.den;
+            return res;
+        }
+       
+        
+    }
+}
---
+++ oop-lab9/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Globalization.CultureInfo customCulture =
+                 (System.Globalization.CultureInfo)
+                  System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Лабораторна робота №9.\nВиконав: Іглінський І.Ю., група ІПЗ-21-4(2)\nВаріант №4.\nЗавдання 1.\n");
+            Console.Title = "Лабораторна робота №9";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Уведіть чисельник(перше число):");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("Уведіть знаменик(перше число):");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("Уведіть чисельник(друге число):");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.Write("Уведіть знаменик(друге число):");
+            int y2 = int.Parse(Console.ReadLine());
+            Fraction a = new Fraction(x1, y1);
+            Fraction b = new Fraction(x2, y2);
+            Console.WriteLine("Перевантажуємо операції");
+            Console.WriteLine("Унарне додавання:");
+            Console.WriteLine(+a);
+            Console.WriteLine("Унарне віднімання:");
+            Console.WriteLine(-a);
+            Console.WriteLine("Бінарне додавання:");
+            Console.WriteLine(a + b);
+            Console.WriteLine("Бінарне віднімання:");
+            Console.WriteLine(a - b);
+            Console.WriteLine("Бінарне множення:");
+            Console.WriteLine(a * b);
+            Console.WriteLine("Бінарне ділення:");
+            Console.WriteLine(a / b);
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.WriteLine("Бінарна операція <");
+            Console.WriteLine(a < b );
+            Console.WriteLine("Бінарна операція >");
+            Console.WriteLine(a > b);
+            Console.WriteLine("Бінарна операція <=");
+            Console.WriteLine(a <= b);
+            Console.WriteLine("Бінарна операція >=");
+            Console.WriteLine(a >= b);
+            Console.WriteLine("Бінарна операція ==");
+            Console.WriteLine(a == b);
+            Console.WriteLine("Бінарна операція !=");
+            Console.WriteLine(a != b);
+            Console.WriteLine("Скорочення дробу:");
+            Fraction.Cut(a);
+            Console.WriteLine("Переведення до double:");
+            Console.WriteLine($"{Fraction.ToDouble(a)}");
+            Console.ResetColor();
+        }
+    }
+}
